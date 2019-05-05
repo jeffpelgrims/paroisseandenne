4d791767d3832547d0daa7d3df0c13f67f016179
Pelgrims Jean-Francois <jeffpelgrims@gmail.com>
2019-05-05 23:31:11 +0200

    Config main pc
    Ajout de la configuration du PC principal

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jeffp\Documents\GitHub\paroisseandenne\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
