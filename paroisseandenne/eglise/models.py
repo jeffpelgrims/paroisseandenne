d67e5816f5e0fd7feb1e2b2ea1d2526168252c03
Pelgrims Jean-Francois <jeffpelgrims@gmail.com>
2019-05-12 08:13:24 +0200

    Some changes
    Adding v0.1 layout, made all the migrations and added some static files.

--- paroisseandenne/eglise/models.py
+++ paroisseandenne/eglise/models.py
@@ -6,6 +6,6 @@ class eglise(models.Model):
     rue = models.CharField(max_length=125)
     numero = models.IntegerField()
     c_postal = models.IntegerField()
-    ville = models.CharField(50)
+    ville = models.CharField(max_length=50)
     description = models.TextField()
-    photo_url = models.CharField(1025)
+    photo_url = models.CharField(max_length=1025)
